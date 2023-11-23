a63de2d5062b45c88a62fc7442638663aeec457c
Ji Yeon <pumpo6969@gmail.com>
2023-11-23 11:02:41 +0700

    sai trai

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "shoe",
-});
-
 // const pool = mysql.createPool({
-//   host: "103.200.23.139",
-//   user: "middmoon_test1",
-//   password: "@M@gj]kiCD9n",
-//   database: "middmoon_ShoeStore",
+//   host: "localhost",
+//   user: "root",
+//   password: "1234",
+//   database: "shoe",
 // });
+
+const pool = mysql.createPool({
+  host: "103.200.23.139",
+  user: "middmoon_test1",
+  password: "@M@gj]kiCD9n",
+  database: "middmoon_ShoeStore",
+});
 
 module.exports = pool;
